b424224f610cc380862cf6c9478ec43bc71cc515
Mieboymesias <161265927+Mieboymesias@users.noreply.github.com>
2025-03-31 10:43:59 +0800

    TASK FORCE
    TASK FORCE

---
+++ data/TASKFORCEACTIVITY_7.js
@@ -0,0 +1,8 @@
+var json_TASKFORCEACTIVITY_7 = {
+"type": "FeatureCollection",
+"name": "TASKFORCEACTIVITY_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "photo": "D:\\HR REPORTS\\AS OF APRIL 4\\BO NITE LOADING OF WASTE MRF.jpg", "TITLE OF ACTIVITY": "LOADING OF WASTE MATERIALS AT MRF BRGY. WASHINGTON SURIGAO CITY", "longitude": "125.48833369444445", "latitude": "9.785663", "TASK FORCE NAME": "BONITE", "ACTIVITIES": "LOADING OF WASTE GARBAGES WITH IN THE BRGY", "DATE": "MARCH 31, 2025", "LENGTH": null }, "geometry": { "type": "Point", "coordinates": [ 125.488333694444449, 9.785663 ] } }
+]
+}
